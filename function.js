25cd0f258f1da9042f9fa56460645a6d6f703922
Hma <93362633+himaladin@users.noreply.github.com>
2024-03-25 04:46:25 +0700

    function.js

--- function.js
+++ function.js
@@ -165,6 +165,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 		// Menambahkan nomor halaman pada PDF
 		opt.jsPDF.autotable = {
 		addPageContent: function(data) {
+		var pdf = data.doc;
 		var pageCount = pdf.internal.getNumberOfPages();
 		pdf.setFontSize(10);
 		pdf.text('Page ' + data.pageNumber + ' of ' + pageCount, data.settings.margin.left, pdf.internal.pageSize.height - 10);
