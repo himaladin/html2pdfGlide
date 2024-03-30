c28ad3dfbe24148e1c13b258b980ca90a326f610
Hma <93362633+himaladin@users.noreply.github.com>
2024-03-30 21:37:12 +0700

    function.js

--- function.js
+++ function.js
@@ -88,8 +88,13 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 
 	const customCSS = `
 	body {
-	  margin: 0 !important
+	  margin: 0!important
 	}
+	.header {
+	  position: relative;
+	  width: 100%;
+	  text-align: center;
+	}	
 	.letterhead {
 	  position: fixed;
 	  top: 0;
@@ -97,6 +102,9 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  transform: translateX(-50%);
 	  width: auto;
 	  z-index: 1;
+	}
+	#content {
+	margin-top: 100px;
 	}
 	button#download {
 	  position: fixed;
