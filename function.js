7a292257b3035034aeeb32daf7e66e7bac8af9be
Hma <93362633+himaladin@users.noreply.github.com>
2024-03-31 10:09:18 +0700

    function.js

--- function.js
+++ function.js
@@ -169,9 +169,10 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	const originalHTML = `
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
 <style>${customCSS}</style>
-<div class="main">
+var htmlContent = `
 <div class="main">
     <div class="header">
+        ${letterheadUrl.trim() !== '' ? `<img src="${letterheadUrl}" class="letterhead" />` : ''}
         <button class="button" id="download">Download</button>
     </div>
     <div id="content">${html}</div>
