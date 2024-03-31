b858b7f70cb3f8bce24fc65f451864406625e019
Hma <93362633+himaladin@users.noreply.github.com>
2024-03-31 16:57:46 +0700

    function.js

--- function.js
+++ function.js
@@ -103,41 +103,49 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  max-width: 1120px;
 	  height: auto;
 	}
- 
+
 .button {
-  padding: 0.6rem 4rem;
-  border: none;
-  outline: none;
-  font-size: 1.3rem;
-  border-radius: 0.3rem;
-  font-weight: 600;
-  background-color: rgba(255, 255, 255, 0.953);
-  box-shadow: 2px 4px 10px -3px rgba(0,0,0,0.27);
-  position: relative;
-  overflow: hidden;
-  cursor: pointer;
-  transition: 0.4s ease-in-out;
+    padding: 0.6rem 4rem;
+    border: none;
+    outline: none;
+    font-size: 1.3rem;
+    border-radius: 0.3rem;
+    font-weight: 600;
+    background-color: rgba(255, 255, 255, 0.953);
+    box-shadow: 2px 4px 10px -3px rgba(0,0,0,0.27);
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    transition: 0.4s ease-in-out;
+}
+
+.button .download-text {
+    position: absolute;
+    left: 1.8rem;
+    top: 1.3rem;
+    transition: 0.4s ease-in-out;
+    color: rgb(50, 50, 50);
 }
 
 .button .svg {
-  transform: translateY(-20px) rotate(30deg);
-  opacity: 0;
-  width: 2rem;
-  transition: 0.4s ease-in-out;
+    transform: translateY(-20px) rotate(30deg);
+    opacity: 0;
+    width: 2rem;
+    transition: 0.4s ease-in-out;
 }
 
 .button:hover {
-  background-color: rgb(50, 50, 50);
+    background-color: rgb(50, 50, 50);
 }
 
 .button:hover .svg {
-  display: inline-block;
-  transform: translateY(0px) rotate(0deg);
-  opacity: 1;
+    display: inline-block;
+    transform: translateY(0px) rotate(0deg);
+    opacity: 1;
 }
 
-.button:active {
-  scale: 0.97;
+.button:hover .download-text {
+    opacity: 0;
 }
 
  	::-webkit-scrollbar {
@@ -158,8 +166,8 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
     <div class="main">
         <div class="header">
             ${letterheadUrl ? `<img src="${letterheadUrl}" class="letterhead"/>` : `<img src="empty-image.png" class="letterhead empty"/>`}
-		<button id="download" class="button">
-		  Download
+	       <button id="download" class="button">
+		  <span class="download-text">Download</span>
 		  <div class="svg">
 		    <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-download" viewBox="0 0 16 16">
 		      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"></path>
@@ -189,8 +197,8 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	            hotfixes: ['px_scaling']
 	        },
 	    };
+	button.disabled = true; // Menonaktifkan tombol selama proses download
 	    button.innerText = 'Downloading...';
-	    button.className = 'downloading';
 	
 	    var content = document.getElementById('content');
 	    var letterheadUrl = '${letterheadUrl}';
@@ -210,8 +218,9 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	            button.innerText = 'Downloaded';
 	            button.className = 'downloaded';
 	            setTimeout(function() {
-	                button.innerText = 'Download PDF';
-	                button.className = '';
+                button.innerText = 'Download PDF';
+                button.disabled = false;
+                button.className = '';
 	                if (letterheadAdded) {
 	                    content.removeChild(content.querySelector('.letterhead'));
 	                }
