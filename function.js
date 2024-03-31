aea03853e41832f1e50f6c912e6c6d35d2b9b5e4
Hma <93362633+himaladin@users.noreply.github.com>
2024-03-31 14:24:08 +0700

    function.js

--- function.js
+++ function.js
@@ -103,104 +103,105 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  max-width: 1120px;
 	  height: auto;
 	}
-button#download {
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-  outline: none;
-  border: 0;
-  vertical-align: middle;
-  text-decoration: none;
-  background: transparent;
-  padding: 0;
-  font-size: inherit;
-  font-family: Arial; 
-  width: 14rem; 
-  height: 3rem; 
-}
-
-button#download .circle {
-  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
-  position: relative;
-  display: block;
-  margin: 0;
-  width: 3rem;
-  height: 3rem;
-  background: #282936;
-  border-radius: 1.625rem;
-}
-
-button#download .circle .icon {
-  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  background: #fff;
-}
-
-button#download .circle .icon.arrow {
-  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
-  left: 0.625rem;
-  width: 1.125rem;
-  height: 0.125rem;
-  background: none;
-}
-
-button#download .circle .icon.arrow::before {
-  position: absolute;
-  content: "";
-  top: -0.29rem;
-  right: 0.0625rem;
-  width: 0.625rem;
-  height: 0.625rem;
-  border-top: 0.125rem solid #fff;
-  border-right: 0.125rem solid #fff;
-  transform: rotate(45deg);
-}
-
-button#download .button-text {
-  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  padding: 0.75rem 0;
-  margin: 0 0 0 2rem;
-  color: #282936;
-  font-weight: 700;
-  line-height: 1.6;
-  text-align: center;
-  text-transform: uppercase;
-}
-
-button#download.downloading {
-  display: block;
-  margin: 0;
-  color: #fff;
-  width: 14rem; 
-  height: 3rem;
-  background: #282936;
-  border-radius: 1.625rem;
-}
-
-button#download.downloading .button-text {
-  color: #fff;
-}
-
-button#download.downloading:hover .circle {
-  width: 100%;
-}
-
-button#download.downloading:hover .circle .icon.arrow {
-  background: #fff;
-  transform: translate(1rem, 0);
-}
-
-button#download.downloading:hover .button-text {
-  color: #fff;
-}
+ 
+	button#download {
+	  position: relative;
+	  display: inline-block;
+	  cursor: pointer;
+	  outline: none;
+	  border: 0;
+	  vertical-align: middle;
+	  text-decoration: none;
+	  background: transparent;
+	  padding: 0;
+	  font-size: inherit;
+	  font-family: Arial; 
+	  width: 14rem; 
+	  height: 3rem; 
+	}
+	
+	button#download .circle {
+	  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+	  position: relative;
+	  display: block;
+	  margin: 0;
+	  width: 3rem;
+	  height: 3rem;
+	  background: #282936;
+	  border-radius: 1.625rem;
+	}
+	
+	button#download .circle .icon {
+	  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+	  position: absolute;
+	  top: 0;
+	  bottom: 0;
+	  margin: auto;
+	  background: #fff;
+	}
+	
+	button#download .circle .icon.arrow {
+	  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+	  left: 0.625rem;
+	  width: 1.125rem;
+	  height: 0.125rem;
+	  background: none;
+	}
+	
+	button#download .circle .icon.arrow::before {
+	  position: absolute;
+	  content: "";
+	  top: -0.29rem;
+	  right: 0.0625rem;
+	  width: 0.625rem;
+	  height: 0.625rem;
+	  border-top: 0.125rem solid #fff;
+	  border-right: 0.125rem solid #fff;
+	  transform: rotate(45deg);
+	}
+	
+	button#download .button-text {
+	  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  padding: 0.75rem 0;
+	  margin: 0 0 0 2rem;
+	  color: #282936;
+	  font-weight: 700;
+	  line-height: 1.6;
+	  text-align: center;
+	  text-transform: uppercase;
+	}
+	
+	button#download.downloading {
+	  display: block;
+	  margin: 0;
+	  color: #fff;
+	  width: 14rem; 
+	  height: 3rem;
+	  background: #282936;
+	  border-radius: 1.625rem;
+	}
+	
+	button#download.downloading .button-text {
+	  color: #fff;
+	}
+	
+	button#download.downloading:hover .circle {
+	  width: 100%;
+	}
+	
+	button#download.downloading:hover .circle .icon.arrow {
+	  background: #fff;
+	  transform: translate(1rem, 0);
+	}
+	
+	button#download.downloading:hover .button-text {
+	  color: #fff;
+	}
 
 	::-webkit-scrollbar {
 	  width: 5px;
