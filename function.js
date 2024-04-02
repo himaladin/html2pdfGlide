848d41be288b285bf3bd8097d55c7b0198f37a37
Hma <93362633+himaladin@users.noreply.github.com>
2024-04-02 13:36:48 +0700

    Update function.js

--- function.js
+++ function.js
@@ -232,7 +232,6 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
                     var pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
                     var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
                     pdf.text(pageWidth - (${margin} + 70), pageHeight - 30, 'Page ' + i + ' of ' + pageCount);
-                    pdf.addImage(footerImg, 'PNG', 0, dimensions.pageHeight - 50, 50, 50);
                 }
 
                 pdf.save('${fileName}.pdf');
