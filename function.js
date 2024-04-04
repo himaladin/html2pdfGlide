84c8cb3fb67acf9fd9853297895c4d4206b2272f
Hma <93362633+himaladin@users.noreply.github.com>
2024-04-04 21:36:35 +0700

    Update function.js

--- function.js
+++ function.js
@@ -204,23 +204,28 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
         var footerImageUrl = '${footerImageUrl}';
 
         setTimeout(function() {
-            html2pdf().set(opt).from(content).toPdf().get('pdf').then(function(pdf) {
-                var pageCount = pdf.internal.getNumberOfPages();
-                // Loop through each page
-                for (var i = 1; i <= pageCount; i++) {
-                    pdf.setPage(i);
-                    pdf.setFontStyle("medium");
-                    pdf.setFontSize(12);
-                    var pageSize = pdf.internal.pageSize;
-                    var pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
-                    var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
-                    pdf.text(pageWidth - (${margin} + 70), pageHeight - 30, 'Page ' + i + ' of ' + pageCount);
-                }
-
-                pdf.save('${fileName}.pdf');
-                button.innerText = 'Downloaded';
-                button.className = 'downloaded';
-            });
+        html2pdf().set(opt).from(content).toPdf().get('pdf').then(function(pdf) {
+            var pageCount = pdf.internal.getNumberOfPages();
+        
+            // Loop through each page
+            for (var i = 1; i <= pageCount; i++) {
+                pdf.setPage(i);
+        
+                // Add letterhead
+                var letterheadImg = new Image();
+                letterheadImg.src = 'URL_GAMBAR_LETTERHEAD';
+                pdf.addImage(letterheadImg, 'PNG', 10, 10, 100, 40); // Ubah posisi dan ukuran sesuai kebutuhan
+        
+                // Add footer image
+                var footerImg = new Image();
+                footerImg.src = 'URL_GAMBAR_FOOTER';
+                pdf.addImage(footerImg, 'PNG', 10, pdf.internal.pageSize.height - 50, 100, 40); // Ubah posisi dan ukuran sesuai kebutuhan
+            }
+        
+            pdf.save('${fileName}.pdf');
+            button.innerText = 'Downloaded';
+            button.className = 'downloaded';
+        });
         }, 1000);
     }, false);
     </script>
