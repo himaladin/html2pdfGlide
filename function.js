243169f0c78bd5bd988ab72f8cc3b0c318cb2530
Hma <93362633+himaladin@users.noreply.github.com>
2024-04-11 11:51:39 +0700

    Update function.js

--- function.js
+++ function.js
@@ -179,54 +179,58 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
         </div>
     </div>
     <script>
-    document.getElementById('download').addEventListener('click', function() {
-        var button = this;
-        var opt = {
-            margin: ${margin},
-            filename: '${fileName}',
-            html2canvas: {
-                useCORS: true,
-                scale: ${quality}
-            },
-            jsPDF: {
-                unit: 'px',
-                orientation: '${orientation}',
-                format: [${finalDimensions}],
-                hotfixes: ['px_scaling']
-            },
-        };
-        button.innerText = 'Downloading...';
-        button.className = 'downloading';
+document.getElementById('download').addEventListener('click', function() {
+    var button = this;
+    var opt = {
+        margin: ${margin},
+        filename: '${fileName}',
+        html2canvas: {
+            useCORS: true,
+            scale: ${quality}
+        },
+        jsPDF: {
+            unit: 'px',
+            orientation: '${orientation}',
+            format: [${finalDimensions}],
+            hotfixes: ['px_scaling']
+        },
+    };
+    button.innerText = 'Downloading...';
+    button.className = 'downloading';
 
-        setTimeout(function() {
-            html2pdf().set(opt).from(document.getElementById('content')).toPdf().get('pdf').then(function(pdf) {
-                var pageCount = pdf.internal.getNumberOfPages();
-                for (var i = 1; i <= pageCount; i++) {
-                    pdf.setPage(i);
-                    pdf.setFontStyle("medium");
-                    pdf.setFontSize(12);
-                    var pageSize = pdf.internal.pageSize;
-                    var pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
-                    var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
-                    pdf.text(pageWidth - (80 + 70), pageHeight - 30, 'Page ' + i + ' of ' + pageCount);
-                    
-                    // Add letterhead image to the first page only
-                    if (i === 1) {
-                        pdf.addImage(letterheadUrl, 'PNG', 40, 30, 60, 60);
-                    }
-                    // Add footer image to every page
-                    pdf.addImage(footerImageUrl, 'PNG', 40, pageHeight - 90, 60, 60);
+    // Get header (letterhead) and footer image URLs from inputs
+    var letterheadUrl = document.getElementById('letterheadUrl').value;
+    var footerImageUrl = document.getElementById('footerImageUrl').value;
+
+    setTimeout(function() {
+        html2pdf().set(opt).from(document.getElementById('content')).toPdf().get('pdf').then(function(pdf) {
+            var pageCount = pdf.internal.getNumberOfPages();
+            for (var i = 1; i <= pageCount; i++) {
+                pdf.setPage(i);
+                pdf.setFontStyle("medium");
+                pdf.setFontSize(12);
+                var pageSize = pdf.internal.pageSize;
+                var pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
+                var pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
+                pdf.text(pageWidth - (80 + 70), pageHeight - 30, 'Page ' + i + ' of ' + pageCount);
+                
+                // Add letterhead image to the first page only
+                if (i === 1) {
+                    pdf.addImage(letterheadUrl, 'PNG', 40, 30, 60, 60);
                 }
-                pdf.save('${fileName}.pdf');
-                button.innerText = 'Downloaded';
-                button.className = 'downloaded';
-            }).catch(function(error) {
-                console.error('Error generating PDF:', error);
-                button.innerText = 'Error';
-                button.className = 'error';
-            });
-        }, 1000);
-    }, false);
+                // Add footer image to every page
+                pdf.addImage(footerImageUrl, 'PNG', 40, pageHeight - 90, 60, 60);
+            }
+            pdf.save('${fileName}.pdf');
+            button.innerText = 'Downloaded';
+            button.className = 'downloaded';
+        }).catch(function(error) {
+            console.error('Error generating PDF:', error);
+            button.innerText = 'Error';
+            button.className = 'error';
+        });
+    }, 1000);
+}, false);
     </script>
     `;
 
