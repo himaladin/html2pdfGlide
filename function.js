dda82a47d7244f15c0e620cad420cc0231f4de92
Hma <93362633+himaladin@users.noreply.github.com>
2024-05-06 21:11:37 +0700

    Update function.js

--- function.js
+++ function.js
@@ -134,7 +134,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
       border: none;
       border-radius: 0.5em;
       color: #DEDEDE;
-      box-shadow: 0.5em 0.5em 0.5em rgba(0, 0, 0, 0.3);
+      box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
       cursor: pointer;
       z-index: 999;
     }
@@ -148,7 +148,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
       left: 0;
       background: linear-gradient(135deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(150,4,31,1) 50%, rgba(191,4,38,1) 60%);
       border-radius: 0 0 0.5em 0;
-      box-shadow: 0.2em 0.2em 0.2em rgba(0, 0, 0, 0.3);
+      box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
       transition: 0.3s;
     }
     
